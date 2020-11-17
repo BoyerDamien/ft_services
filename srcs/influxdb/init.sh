a0a655f1aec99360cda2c84b9d8176634bb791cd
BoyerDamien <dboyer@student.42.fr>
2020-11-17 20:44:43 +0100

    influxdb and telegraf work

--- srcs/influxdb/init.sh
+++ srcs/influxdb/init.sh
@@ -1,8 +1,16 @@
 #! /bin/sh
 
-if [ ! -d /root/.influxdb/data ]
+influxd & jobs
+
+if [ ! -f /root/.influxdbv2/configs ]
 then
-	influxd & influx setup -u user -p password -o 42  -b ft_services --retention=1d --force
-else
-	influxd
+	sleep 10 && influx setup -u $DB_USER\
+		-p $DB_PASSWORD\
+		-o $INFLUX_ORGANIZATION\
+		-b $INFLUX_BUCKET\
+		-t $INFLUX_TOKEN\
+		--retention=1d --force
+	influx telegrafs create --name $INFLUX_BUCKET --description $INFLUX_BUCKET --file telegraf.conf
 fi
+
+telegraf --config telegraf.conf
--- srcs/influxdb/start.sh
+++
@@ -1,4 +0,0 @@
-#! /bin/sh
-
-curl -X POST 'http://localhost:8086/query' --data-urlencode "q=CREATE DATABASE telegraf"
-curl -X POST 'http://localhost:8086/query' --data-urlencode "q=CREATE USER user WITH PASSWORD 'password'"
