1e073ce5cc6069a6986c94b2ee5a8ab9471214dd
BoyerDamien <dboyer@student.42.fr>
2020-11-19 16:26:37 +0100

    dashboard ok

--- srcs/grafana/init.sh
+++ srcs/grafana/init.sh
@@ -4,6 +4,8 @@ if [ ! -d /usr/share/grafana/data ]
 then
 	apk add --no-cache grafana --repository http://dl-3.alpinelinux.org/alpine/edge/testing/
 	mv /usr/sbin/grafana-server . && mv /usr/bin/grafana-cli .
+	mv /dashboard1.json /usr/share/grafana/conf/provisioning/dashboards
+	mv /dashboard1.yaml /usr/share/grafana/conf/provisioning/dashboards
 	./grafana-cli admin reset-admin-password $ADMIN_PASSWORD
 fi
 
